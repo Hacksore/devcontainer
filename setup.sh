2f656edc2af1519636fd95c990b85db0b855b97a
Sean Boult <996134+Hacksore@users.noreply.github.com>
2022-10-22 17:56:04 -0500

    Test

--- setup.sh
+++ setup.sh
@@ -6,33 +6,23 @@ apt update
 # native deps for brew
 apt install -y curl git
 
-# clone the repo with dotfiles if not exists
-if [ -d "$HOME/dotfiles" ]; then
-  git clone https://github.com/Hacksore/dotfiles.git "$HOME/dotfiles"
-fi
+# checkout dotfiles
+git clone https://github.com/Hacksore/dotfiles.git "$HOME/dotfiles"
 
-if [ -d "$HOME/homebrew" ]; then
-  git clone https://github.com/Homebrew/brew "$HOME/homebrew"
+# install homebrew
+git clone https://github.com/Homebrew/brew "$HOME/homebrew"
 
-  cd $HOME
-  eval "$(homebrew/bin/brew shellenv)"
+cd $HOME
+eval "$(homebrew/bin/brew shellenv)"
 
-  brew update --force --quiet
-  chmod -R go-w "$(brew --prefix)/share/zsh"
+brew update --force --quiet
+chmod -R go-w "$(brew --prefix)/share/zsh"
 
-  # install ansible and stow for configuration
-  brew install ansible stow
+# install ansible and stow for configuration
+brew install stow
 
-  # setup the brew mod
-  ansible-galaxy collection install community.general
-
-  # brew just installed let's run our ansible stuff
-  bash "$HOME/dotfiles/ansible/run.sh"
-
-  # use brew bundle
-  brew bundle --file "$HOME/dotfiles/Brewfile"
-
-fi
+# use brew bundle
+brew bundle --file "$HOME/dotfiles/Brewfile"
 
 # install the files with stow
 stow .
