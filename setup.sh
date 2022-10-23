4894b9033cc538a1f645647b8b657b785e92edf8
Sean Boult <996134+Hacksore@users.noreply.github.com>
2022-10-22 21:06:30 -0500

    Update setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export HOMEBREW_PREFIX="/home/linuxbrew/.linuxbrew"
+
 # system update
 apt update
 
@@ -16,7 +18,7 @@ git clone https://github.com/Hacksore/dotfiles.git "$HOME/dotfiles"
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # load brew
-eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+eval $($HOMEBREW_PREFIX/bin/brew shellenv)
 
 # brew deps
 brew install stow zsh fzf nvm zsh-syntax-highlighting zsh-autosuggestions zsh-completions thefuck diff-so-fancy
@@ -25,16 +27,19 @@ brew install stow zsh fzf nvm zsh-syntax-highlighting zsh-autosuggestions zsh-co
 yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # install fzf
-yes | /home/linuxbrew/.linuxbrew/opt/fzf/install
+yes | $HOMEBREW_PREFIX/opt/fzf/install
 
 # remove existing one
 rm $HOME/.zshrc
 
+# load nvm
+[ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ] && \. "$HOMEBREW_PREFIX/opt/nvm/nvm.sh"  # This loads nvm
+
+nvm install 16
+
 # link all dot files
 cd $HOME/dotfiles
 stow .
-
-nvm install 16
 
 # done
 echo "All config files have been linked..."
