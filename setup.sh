59e6b29abb82c439822a0c993caf0793f4baf66d
Sean Boult <996134+Hacksore@users.noreply.github.com>
2022-11-06 20:01:31 +0000

    Test

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export HOMEBREW_PREFIX="/home/linuxbrew/.linuxbrew"
-
 # system update
 apt update
 
@@ -9,46 +7,15 @@ apt update
 useradd -m Hacksore
 
 # native deps for brew
-apt install -y curl git
-
-# checkout dotfiles
-git clone https://github.com/Hacksore/dotfiles.git "$HOME/dotfiles"
-
-# install homebrew
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# load brew
-eval $($HOMEBREW_PREFIX/bin/brew shellenv)
-
-# brew deps
-brew install stow zsh fzf nvm zsh-syntax-highlighting zsh-autosuggestions zsh-completions thefuck diff-so-fancy
-
-# install ohmyzsh
-yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# install fzf
-yes | $HOMEBREW_PREFIX/opt/fzf/install
-
-# remove existing one
-rm $HOME/.zshrc
-
-# load nvm
-[ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ] && \. "$HOMEBREW_PREFIX/opt/nvm/nvm.sh"  # This loads nvm
-
-nvm install 16
-
-# link all dot files
-cd $HOME/dotfiles
-stow .
+apt install -y curl git default-jre
 
 # install yarn
 npm i -g yarn
 
-# install java
-apt install default-jre -y
-
 # add firebase tools
-yarn global add firebase-tools
+yarn global add firebase-tools vercel
+
+touch /root/test.file
 
 # done
-echo "All config files have been linked..."
+echo "Container has been setup 🚀"
